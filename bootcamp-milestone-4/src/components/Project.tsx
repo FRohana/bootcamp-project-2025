cd5703c7cec11ace57e0f486400fb42961ba50cf
Farid Rohana <faridrohana0@gmail.com>
2025-12-10 15:51:34 -0800

    css changes

--- bootcamp-milestone-4/src/components/Project.tsx
+++ bootcamp-milestone-4/src/components/Project.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import style from "./project.module.css";
 import Image from "next/image";
 import Comment from "./Comment";
@@ -28,58 +31,61 @@ export default function Project({
 }: ProjectProps) {
   // Ensure comments is an array
   const projectComments = comments && Array.isArray(comments) ? comments : [];
+  const [showComments, setShowComments] = useState(false);
 
   return (
-    <div className={style.projectWrapper}>
-      {/* Project Card */}
-      <div className={style.project}>
-        <a href={projectUrl} target="_blank" rel="noopener noreferrer">
+    <div className={style.projectCard}>
+      <div className={style.projectContent}>
+        <a href={projectUrl} target="_blank" rel="noopener noreferrer" className={style.imageLink}>
           <Image
             src={imageUrl}
             alt={name}
-            width={300}
-            height={300}
+            width={320}
+            height={220}
             className={style.projectImage}
           />
         </a>
-        <div className={style.projectDetails}>
-          <p className={style.projectName}>
-            <strong>{name}</strong>
-          </p>
+        <div className={style.projectInfo}>
+          <h3 className={style.projectName}>{name}</h3>
           <p className={style.projectDescription}>{description}</p>
-          <a
-            href={projectUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={style.projectLink}
-          >
-            {linkText}
-          </a>
+          <div className={style.projectActions}>
+            <a
+              href={projectUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={style.projectLink}
+            >
+              {linkText}
+            </a>
+            <button
+              onClick={() => setShowComments(!showComments)}
+              className={style.commentsToggle}
+            >
+              {projectComments.length > 0
+                ? `${projectComments.length} ${projectComments.length === 1 ? "Comment" : "Comments"}`
+                : "Comments"}
+            </button>
+          </div>
         </div>
       </div>
 
-      {/* Comments Sidebar */}
-      <aside className={style.commentsSidebar}>
-        <h3 className={style.commentsTitle}>
-          {projectComments.length > 0
-            ? `${projectComments.length} ${
-                projectComments.length === 1 ? "COMMENT" : "COMMENTS"
-              }`
-            : "COMMENTS"}
-        </h3>
-        <div className={style.commentsList}>
-          {projectComments.length > 0 ? (
-            projectComments.map((comment, index) => (
-              <Comment key={index} comment={comment} />
-            ))
-          ) : (
-            <p className={style.noComments}>
-              No comments yet. Be the first to comment!
-            </p>
-          )}
+      {/* Comments Section - Collapsible */}
+      {showComments && (
+        <div className={style.commentsSection}>
+          <div className={style.commentsList}>
+            {projectComments.length > 0 ? (
+              projectComments.map((comment, index) => (
+                <Comment key={index} comment={comment} />
+              ))
+            ) : (
+              <p className={style.noComments}>
+                No comments yet. Be the first to comment!
+              </p>
+            )}
+          </div>
+          <PortfolioCommentForm projectName={name} />
         </div>
-        <PortfolioCommentForm projectName={name} />
-      </aside>
+      )}
     </div>
   );
 }
